5b9b833fc3f55f6d005154879589d4b4725ae7ef
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-05 11:49:36 +0900

    commit

---
+++ 00_YUSEUNGJE/chap18-assertions-lecture-source/src/test/java/com/ohgiraffers/section02/assertj/AssertjTests.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section02.assertj;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/* Test 3rd party lib 중 AssertJ의 사용법에 대해 이해할 수 있다. */
+class AssertjTests {
+
+    /* 1. 문자열 테스트 */
+    @Test
+    @DisplayName("문자열 테스트 하기")
+    void testStringValidation()  {}
+}
