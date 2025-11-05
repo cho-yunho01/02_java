7f6b17c3a694c3a6574d84306c93aac796164952
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-05 10:51:09 +0900

    commit

--- 00_YUSEUNGJE/chap18-assertions-lecture-source/src/test/java/com/ohgiraffers/section1/jupiter/JupiterAssertionsTests.java
+++ 00_YUSEUNGJE/chap18-assertions-lecture-source/src/test/java/com/ohgiraffers/section1/jupiter/JupiterAssertionsTests.java
@@ -1,4 +1,8 @@
+package com.ohgiraffers.section1.jupiter;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+/* JUnit5에서 제공하는 Assertions에 있는 메소드에 대해서 이해하고 활용할 수 있다.*/
 class JupiterAssertionsTests {
-  
+
 }
